c55ee79321cb0605e52561c51396354964cd0271
Harsh Mishra <harshmishra@Harshs-MacBook-Air.local>
2026-05-13 17:24:15 +0530

    Added users API

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const {
   signup,
   login,
 } = require('../controllers/authController');
+
+router.get(
+  '/users',
+  authMiddleware,
+
+  async (req, res) => {
+
+    try {
+
+      const users = await User.find().select(
+        '-password'
+      );
+
+      res.json(users);
+
+    } catch (error) {
+
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  }
+);
 
 router.post('/signup', signup);
 
